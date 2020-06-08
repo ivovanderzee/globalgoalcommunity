920bd68b4b3c4bcc0855ee2c9068721203b3004f
joannie <1387736@hera.nl>
2020-06-08 10:58:51 +0200

    basic styling (wrapper, button en kleur)

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/event',
+    name: 'Event',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Event.vue')
+  },
+  {
+    path: '/solution',
+    name: 'Solution',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Solution.vue')
+  },
+  {
+    path: '/question',
+    name: 'Question',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Question.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
