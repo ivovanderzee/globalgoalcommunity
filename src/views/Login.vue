557124e50621bb1b64ee34cca3fc01187274a257
Vincent,Iris I <i.vincent@student.fontys.nl>
2020-06-08 11:34:59 +0200

    test voor joanne 2

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="login">
         <h1>This is an login page</h1>
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
     </div>
 </template>
+<script>
+    // @ is an alias to /src
+    import HelloWorld from '@/components/HelloWorld.vue'
+
+    export default {
+        name: 'Home',
+        components: {
+            HelloWorld
+        }
+    }
+</script>
